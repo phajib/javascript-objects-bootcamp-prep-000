28f8cf8b30f69be1aae4f201232b1c4d60a19dd9
H B <phajib@gmail.com>
2018-11-09 08:00:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -15,6 +15,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 /*removeFromPlaylist function
 removes artistName from playlist*/
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName;
+  //delete playlist.artistName;
+  delete playlist [artistName]
   return playlist
 }
