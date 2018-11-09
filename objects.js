04de9ed56075ffda8d576bd289c4ca777fd54fc3
H B <phajib@gmail.com>
2018-11-09 07:42:53 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,7 @@
+/*playlist object*/
+var playlist = {artist-song:"Silverchair - Israel's Son"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist.Slowdive = 'Alison'
+  playlist ['My Bloody Valentine'] = 'Sometimes'
+}
